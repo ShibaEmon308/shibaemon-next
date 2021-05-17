d718e8f346428c94fd6fc5770b20935bfeca14ff
Atsuhito <shibaemon.japan+github@gmail.com>
2021-05-18 02:48:36 +0900

    add head

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,6 +1,11 @@
+import Henad from "next/head"
+
 export default function Home() {
   return (
     <div>
+      <head>
+        <title>ShibaEmon NextJS Blog</title>
+      </head>
       <main>Welcome</main>
     </div>
   );
